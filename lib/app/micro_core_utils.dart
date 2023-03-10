8efcb2128b1e037c4e0c0bb9e6146e3d2f33b3b6
Junior Nakamura <juniornakamura@eworldes.com>
2023-03-10 13:30:48 -0300

    Added DI configuration

--- lib/app/micro_core_utils.dart
+++ lib/app/micro_core_utils.dart
@@ -1,4 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:micro_core/app/micro_core_utils.config.dart';
+import 'package:micro_core/app/microapp.dart';
 
 typedef WidgetBuilderArgs = Widget Function(BuildContext context, Object? args);
 late GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final getIt = GetIt.instance;
+
+@InjectableInit(asExtension: false)
+FutureOr<GetIt> configureInjection() => init(getIt);
